11e08232d1a0f3c7c224eb0fe15de92a27330df0
Mohit Talele <mohit.talele@rtl2.de>
2022-05-13 15:01:21 +0200

    update hash function

--- tests/checksum.py
+++ tests/checksum.py
@@ -5,7 +5,7 @@ def md5(fname):
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
-    return hash_md5.digest()
+    return hash_md5.hexdigest()
 
 resmd5 = md5("./validation-data/yoda.jpg")
 print("calculated checksum is ", resmd5)
