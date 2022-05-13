2a1fe57effda9540de9991b92d17137b5100b9f8
Mohit Talele <mohit.talele@rtl2.de>
2022-05-13 14:57:38 +0200

    add dummy checksum

---
+++ tests/checksum.py
@@ -0,0 +1,18 @@
+import hashlib
+import sys
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.digest()
+
+resmd5 = md5("./validation-data/yoda.jpg")
+print("calculated checksum is ", resmd5)
+checksum = "2624daf668b78c8f72c9895318d9703e"
+print("expected checsum is" , checksum)
+if resmd5 == checksum :
+    print( "Checksum matched" )
+    sys.exit(0)
+else :
+    sys.exit(1)
