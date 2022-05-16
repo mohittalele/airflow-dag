cc0a590f38f255e3b415968548b3996b051d3c77
Mohit Talele <mohit.talele@rtl2.de>
2022-05-16 21:56:22 +0200

    add job to publish message

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@
 kubectl create ns airflow
 helmfile template > template.yaml
 kubectl apply -n airflow -f template.yaml
+
+#kubectl create configmap name-of-your-configmap --from-file=utility-functions/message-producer.py  --dry-run=client -o yaml > cm.yaml
