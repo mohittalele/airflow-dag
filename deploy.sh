65f6aaa9d02f3639482c65a11cac7b17c35757f7
Mohit Talele <mohit.talele@rtl2.de>
2022-05-20 15:38:28 +0200

    refactor include file in release

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 kubectl create ns airflow
 helmfile template > template.yaml
 kubectl apply -n airflow -f template.yaml
-
+#helmfile -l app=message-producer template --skip-deps > template.yaml 
 #kubectl create configmap name-of-your-configmap --from-file=utility-functions/message-producer.py  --dry-run=client -o yaml > cm.yaml
