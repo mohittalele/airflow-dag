e1899647c319bc27c1553f70e2c01ea9bc41ad32
Mohit Talele <mohit.talele@rtl2.de>
2022-05-24 18:43:17 +0200

    update code for refactoring

--- DAG/hydra_airflow_test.py
+++ DAG/hydra_airflow_test.py
@@ -1,21 +1,53 @@
-import hydra
-from airflow.decorators import dag, task
+from typing import Any, Dict
+
+import httpx
 import pendulum
 
+from airflow.decorators import dag, task
+from airflow.models.baseoperator import BaseOperator
+from airflow.operators.email import EmailOperator
+from airflow.utils.context import Context
 
+
+class GetRequestOperator(BaseOperator):
+    """Custom operator to send GET request to provided url"""
+
+    def __init__(self, *, url: str, **kwargs):
+        super().__init__(**kwargs)
+        self.url = url
+
+    def execute(self, context: Context):
+        return httpx.get(self.url).json()
+
+
+# [START dag_decorator_usage]
 @dag(
-    dag_id="hydra",
     schedule_interval=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False)
-def dag():
+    catchup=False,
+    tags=['example'],
+)
+def example_dag_decorator(email: str = 'example@example.com'):
+    """
+    DAG to send server IP to email.
 
-    @hydra.main(version_base=None, config_path="../environments", config_name="prod")
-    @task(task_id="print_config")
-    def print_config(cfg):
-        print("db.user :", cfg.db.user)
-        print("db.password :", cfg.db.user)
+    :param email: Email to send IP to. Defaults to example@example.com.
+    """
+    get_ip = GetRequestOperator(task_id='get_ip', url="http://httpbin.org/get")
 
-    print_config_instance = print_config()
+    @task(multiple_outputs=True)
+    def prepare_email(raw_json: Dict[str, Any]) -> Dict[str, str]:
+        external_ip = raw_json['origin']
+        return {
+            'subject': f'Server connected from {external_ip}',
+            'body': f'Seems like today your server executing Airflow is connected from IP {external_ip}<br>',
+        }
 
-hydra_trials = dag()
+    email_info = prepare_email(get_ip.output)
+
+    EmailOperator(
+        task_id='send_email', to=email, subject=email_info['subject'], html_content=email_info['body']
+    )
+
+
+dag = example_dag_decorator()
