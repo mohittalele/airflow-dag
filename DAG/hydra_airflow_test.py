afbf29152db32a740fbe1c5428b9f1265979d831
Mohit Talele <mohit.talele@rtl2.de>
2022-05-24 18:25:57 +0200

    restructure files

--- DAG/hydra_airflow_test.py
+++ DAG/hydra_airflow_test.py
@@ -19,17 +19,3 @@ def dag():
     print_config_instance = print_config()
 
 hydra_trials = dag()
-
-# from omegaconf import DictConfig, OmegaConf
-# import hydra
-#
-#
-# @hydra.main(version_base=None, config_path="../config", config_name="prod")
-# def my_app(cfg):
-#     print(OmegaConf.to_yaml(cfg))
-#     print("db.user :", cfg.db.user)
-#     print("db.password :", cfg.db.user)
-#
-#
-# if __name__ == "__main__":
-#     my_app()
