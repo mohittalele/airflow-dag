d3946528a4dbf0233c828c4a1b2d6077c6bc3480
Mohit Talele <mohit.talele@rtl2.de>
2022-05-24 15:58:06 +0200

    install dependencies on fly

--- DAG/hydra_trials.py
+++ DAG/hydra_trials.py
@@ -13,6 +13,8 @@ def dag():
     @hydra.main(version_base=None, config_path="../environments", config_name="prod")
     @task(task_id="print_config")
     def print_config(cfg):
+        import subprocess
+        subprocess.run(["pip", "install", "omegaconf"])
         from omegaconf import OmegaConf
         print(OmegaConf.to_yaml(cfg))
         print(OmegaConf.to_yaml(cfg))
