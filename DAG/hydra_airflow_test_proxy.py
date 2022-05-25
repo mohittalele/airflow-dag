57ded06c07cfa53f6080f02a7940e11cf2a01145
Mohit Talele <mohit.talele@rtl2.de>
2022-05-25 19:39:00 +0200

    update airflow dag

--- DAG/hydra_airflow_test_proxy.py
+++ DAG/hydra_airflow_test_proxy.py
@@ -1,72 +1,29 @@
-from typing import Any, Dict
+import logging
+import shutil
+import time
+from pprint import pprint
 
-import httpx
 import pendulum
-from airflow.decorators import dag, task
-from airflow.models.baseoperator import BaseOperator
-from airflow.operators.email import EmailOperator
-from airflow.utils.context import Context
-import hydra
-from omegaconf import DictConfig, OmegaConf
 
+from airflow import DAG
+from airflow.decorators import task
 import os
+log = logging.getLogger(__name__)
 
-class GetRequestOperator(BaseOperator):
-    """Custom operator to send GET request to provided url"""
-
-    def __init__(self, *, url: str, **kwargs):
-        super().__init__(**kwargs)
-        self.url = url
-
-    def execute(self, context: Context):
-        return httpx.get(self.url).json()
-
-
-# [START dag_decorator_usage]
-@dag(
+with DAG(
+    dag_id='example_python_operator',
     schedule_interval=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
     tags=['example'],
-)
-def example_dag_decorator_proxy(email: str = 'example@example.com'):
-    """
-    DAG to send server IP to email.
+) as dag:
+    # [START howto_operator_python]
+    @task(task_id="print_the_context")
+    def print_context(ds=None, **kwargs):
+        """Print the Airflow context and ds variable from the context."""
+        print(os.getcwd())
+        pprint(kwargs)
+        print(ds)
+        return 'Whatever you return gets printed in the logs'
 
-    :param email: Email to send IP to. Defaults to example@example.com.
-    """
-
-    CONFIG_PATH = os.path.abspath(os.path.join(__file__,'environments'))
-
-    get_ip = GetRequestOperator(task_id='get_ip', url="http://httpbin.org/get")
-
-    @hydra.main(version_base=None, config_path=CONFIG_PATH, config_name="dev")
-    @task(multiple_outputs=True)
-    def get_conf(cfg: DictConfig) -> Dict[str, DictConfig]:
-        print(OmegaConf.to_yaml(cfg))
-        print("db.user :", cfg.db.user)
-        print("db.password :", cfg.db.user)
-        return {'configuration': cfg}
-
-    get_conf_instance = get_conf()
-
-    @task(multiple_outputs=True)
-    def prepare_email(raw_json: Dict[str, Any], raw_conf: Dict[str, DictConfig]) -> Dict[str, str]:
-        external_ip = raw_json['origin']
-        cfg = raw_conf['configuration']
-        print(OmegaConf.to_yaml(cfg))
-        print("db.user :", cfg.db.user)
-        print("db.password :", cfg.db.user)
-        return {
-            'subject': f'Server connected from {external_ip}',
-            'body': f'Seems like today your server executing Airflow is connected from IP {external_ip}<br>',
-        }
-
-    email_info = prepare_email(get_ip.output, get_conf_instance)
-
-    EmailOperator(
-        task_id='send_email', to=email, subject=email_info['subject'], html_content=email_info['body']
-    )
-
-
-dag = example_dag_decorator_proxy()
+    run_this = print_context()
