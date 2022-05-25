a444fca3992623f33f3aa53e15a16aa7a20454fa
Mohit Talele <mohit.talele@rtl2.de>
2022-05-25 12:18:26 +0200

    add dag

--- DAG/hydra_airflow_test_2.py
+++ DAG/hydra_airflow_test_2.py
@@ -6,8 +6,6 @@ from airflow.decorators import dag, task
 from airflow.models.baseoperator import BaseOperator
 from airflow.operators.email import EmailOperator
 from airflow.utils.context import Context
-import hydra
-from omegaconf import DictConfig, OmegaConf
 
 
 class GetRequestOperator(BaseOperator):
@@ -36,29 +34,29 @@ def example_dag_decorator_2(email: str = 'example@example.com'):
     """
     get_ip = GetRequestOperator(task_id='get_ip', url="http://httpbin.org/get")
 
-    @hydra.main(version_base=None, config_path="environments/", config_name="dev")
-    @task(multiple_outputs=True)
-    def get_conf(cfg: DictConfig) -> Dict[str, DictConfig]:
-        print(OmegaConf.to_yaml(cfg))
-        print("db.user :", cfg.db.user)
-        print("db.password :", cfg.db.user)
-        return {'configuration': cfg}
+    # @hydra.main(version_base=None, config_path="environments/", config_name="dev")
+    # @task(multiple_outputs=True)
+    # def get_conf(cfg: DictConfig) -> Dict[str, DictConfig]:
+    #     print(OmegaConf.to_yaml(cfg))
+    #     print("db.user :", cfg.db.user)
+    #     print("db.password :", cfg.db.user)
+    #     return {'configuration': cfg}
+    #
+    # get_conf_instance = get_conf()
 
-    get_conf_instance = get_conf()
-
     @task(multiple_outputs=True)
-    def prepare_email(raw_json: Dict[str, Any], raw_conf: Dict[str, DictConfig]) -> Dict[str, str]:
+    def prepare_email(raw_json: Dict[str, Any]) -> Dict[str, str]:
         external_ip = raw_json['origin']
-        cfg = raw_conf['configuration']
-        print(OmegaConf.to_yaml(cfg))
-        print("db.user :", cfg.db.user)
-        print("db.password :", cfg.db.user)
+        # cfg = raw_conf['configuration']
+        # print(OmegaConf.to_yaml(cfg))
+        # print("db.user :", cfg.db.user)
+        # print("db.password :", cfg.db.user)
         return {
             'subject': f'Server connected from {external_ip}',
             'body': f'Seems like today your server executing Airflow is connected from IP {external_ip}<br>',
         }
 
-    email_info = prepare_email(get_ip.output, get_conf_instance)
+    email_info = prepare_email(get_ip.output)
 
     EmailOperator(
         task_id='send_email', to=email, subject=email_info['subject'], html_content=email_info['body']
