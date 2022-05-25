6a52d6781126d147e7d9c371d1ee7e06c6dd468d
Mohit Talele <mohit.talele@rtl2.de>
2022-05-25 19:50:33 +0200

    reformat file

--- DAG/hydra_airflow_test_proxy.py
+++ DAG/hydra_airflow_test_proxy.py
@@ -8,17 +8,23 @@ import pendulum
 from airflow import DAG
 from airflow.decorators import task
 import os
+import hydra
 log = logging.getLogger(__name__)
 
 with DAG(
-    dag_id='example_python_operator',
-    schedule_interval=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    tags=['example'],
+        dag_id='example_python_operator',
+        schedule_interval=None,
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        tags=['example'],
 ) as dag:
     # [START howto_operator_python]
+
+    CONFIG_PATH = os.path.abspath(os.path.join(__file__, '..', 'environments'))
+
+
     @task(task_id="print_the_context")
+    @hydra.main(version_base=None, config_path=CONFIG_PATH, config_name="dev")
     def print_context(ds=None, **kwargs):
         """Print the Airflow context and ds variable from the context."""
         CONFIG_PATH = os.path.abspath(os.path.join(__file__, '..', 'environments'))
@@ -27,5 +33,6 @@ with DAG(
         pprint(kwargs)
         print(ds)
         return 'Whatever you return gets printed in the logs'
+
 
     run_this = print_context()
