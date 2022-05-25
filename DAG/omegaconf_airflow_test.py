26081b204f3ea588d3e80792d01d7365ea7ebb1e
Mohit Talele <mohit.talele@rtl2.de>
2022-05-25 20:25:43 +0200

    remove bash operator dag

--- DAG/omegaconf_airflow_test.py
+++ DAG/omegaconf_airflow_test.py
@@ -14,7 +14,7 @@ from omegaconf import OmegaConf
 log = logging.getLogger(__name__)
 
 with DAG(
-        dag_id='example_python_operator',
+        dag_id='omegaconf_airflow_test',
         schedule_interval=None,
         start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
         catchup=False,
@@ -26,17 +26,17 @@ with DAG(
     env = 'prod.yaml'
     CONFIG_PATH_1 = os.path.abspath(os.path.join(__file__, '..', 'environments/', env))
     print("NEW CONFIG_PATH", CONFIG_PATH_1)
+    omega_cfg_1 = OmegaConf.load(CONFIG_PATH_1)
 
-
-    @task(task_id="omegaconf_airflow_test")
-    def print_context( ds=None, **kwargs):
+    @task(task_id="omegaconf_test")
+    def print_context(ds=None, **kwargs):
         """Print the Airflow context and ds variable from the context."""
 
         print("CONFIG_PATH :", CONFIG_PATH)
         print("CONFIG_PATH_1 :", CONFIG_PATH_1)
 
         print("------- omegaconf---------------")
-        omega_cfg_1 = OmegaConf.load(CONFIG_PATH_1)
+
         print(OmegaConf.to_yaml(omega_cfg_1))
         print("db.user :", omega_cfg_1.db.user)
         print("db.password :", omega_cfg_1.db.password)
@@ -46,7 +46,7 @@ with DAG(
         print("------- omegaconf---------------")
         pprint(kwargs)
         print(ds)
-        return omega_cfg_1
+        return 'Whatever you return gets printed in the logs'
 
 
     run_this = print_context()
