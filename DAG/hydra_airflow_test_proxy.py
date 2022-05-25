df89f402654764014cb0fc0508f391f94c81ca1e
Mohit Talele <mohit.talele@rtl2.de>
2022-05-25 19:43:43 +0200

    add debugging

--- DAG/hydra_airflow_test_proxy.py
+++ DAG/hydra_airflow_test_proxy.py
@@ -21,7 +21,9 @@ with DAG(
     @task(task_id="print_the_context")
     def print_context(ds=None, **kwargs):
         """Print the Airflow context and ds variable from the context."""
-        print(os.getcwd())
+        CONFIG_PATH = os.path.abspath(os.path.join(__file__, '..', 'environments'))
+        print("CONFIG_PATH :", CONFIG_PATH)
+        print("current work dir :", os.getcwd())
         pprint(kwargs)
         print(ds)
         return 'Whatever you return gets printed in the logs'
