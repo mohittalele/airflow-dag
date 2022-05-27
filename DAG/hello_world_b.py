55ff7fdfa65f534a215bca423b010ca6e1b6e463
Mohit Talele <mohit.talele@rtl2.de>
2022-05-27 13:51:47 +0200

    add logs

--- DAG/hello_world_b.py
+++ DAG/hello_world_b.py
@@ -19,8 +19,10 @@ def print_config():
     print(OmegaConf.to_yaml(res))
     print("CONFIG_PATH : ", config_path_dev)
     print("res.db.jobs = ", res.db.jobs)
-    print("res.db.jobs = ", res.db.vyper_settings)
+    print("res.db.vyper_setting = ", res.db.vyper_settings)
     print("res.db = ", res.db)
+    print("db.vyper_setting.tagger.output_bucket_path" , res.db.vyper_setting.tagger.output_bucket_path)
+    print("db.vyper_setting.slang_word_tagger.output_bucket_path" , res.db.vyper_setting.slang_word_tagger.output_bucket_path)
     print("Sleeping the task for 5 minutes")
     time.sleep(300.0)
 
