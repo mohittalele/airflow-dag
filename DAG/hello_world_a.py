7027239a7dbfd0d64aebc4990b43c0db05b4df7e
Mohit Talele <mohit.talele@rtl2.de>
2022-05-27 12:12:27 +0200

    upadate hello world task to load conf POC

--- DAG/hello_world_a.py
+++ DAG/hello_world_a.py
@@ -1,10 +1,27 @@
+import os
+
+
 import airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 
+
 def print_hello(dag_run=None):
     # task_params = context['dag_run'].conf['task_payload']
+    from omegaconf import OmegaConf
+    import time
+
+    config_path_default = os.path.abspath(os.path.join(__file__, '..', 'environments/default.yaml'))
+    config_path_dev = os.path.abspath(os.path.join(__file__, '..', 'environments/dev', 'dev.yaml'))
+    omega_cfg_dev = OmegaConf.load(config_path_dev)
+    omega_cfg_default = OmegaConf.load(config_path_default)
+    res = OmegaConf.merge(omega_cfg_default, omega_cfg_dev)
+    res.db.UDID = "ABCDEF_Task_a"
+    res.db.date = time.strftime("%Y%m%d-%H%M%S")
+    print(OmegaConf.to_yaml(res))
+
+    print("NEW CONFIG_PATH_2 : ", config_path_dev)
     print(f"Remotely received value of {dag_run.conf.get('job_params')} for key=job_params")
     # print('Hello world a with {}'.format({dag_run.conf.get('job_params')}))
 
--- DAG/hello_world_b.py
+++ DAG/hello_world_b.py
@@ -1,3 +1,5 @@
+
+
 import airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -7,7 +9,18 @@ def print_hello(dag_run=None):
     # task_params = context['dag_run'].conf['task_payload']
     print(f"Remotely received value of {dag_run.conf.get('job_params')} for key=job_params")
     # print('Hello world a with {}'.format({dag_run.conf.get('job_params')}))
+    from omegaconf import OmegaConf
+    import time
+    import os
 
+    config_path_default = os.path.abspath(os.path.join(__file__, '..', 'environments/default.yaml'))
+    config_path_dev = os.path.abspath(os.path.join(__file__, '..', 'environments/dev', 'dev.yaml'))
+    omega_cfg_dev = OmegaConf.load(config_path_dev)
+    omega_cfg_default = OmegaConf.load(config_path_default)
+    res = OmegaConf.merge(omega_cfg_default, omega_cfg_dev)
+    res.db.UDID = "ABCDEF_Task_b"
+    res.db.date = time.strftime("%Y%m%d-%H%M%S")
+    print(OmegaConf.to_yaml(res))
 
 with DAG(
         dag_id='hello_world_b',
