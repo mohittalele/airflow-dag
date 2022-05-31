dd85310a31a8d9e2202757584519d6acadddbd7a
Mohit Talele <mohit.talele@rtl2.de>
2022-05-31 17:24:03 +0200

    gracefully close rabbitmq connection

--- DAG/external_trigger1.py
+++ DAG/external_trigger1.py
@@ -69,14 +69,16 @@ def consume_message(**kwargs):
             channel.basic_ack(delivery_tag=method_frame.delivery_tag)
             connection.close()
             print("Got message ? {}".format(body))
+            connection.close(reply_code="200", reply_text="Gracefully closed connection")
             return json_params['task']
         elif 'Key' in json_params:
             kwargs['ti'].xcom_push(key='message', value=json.dumps(json_params))
             channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-            connection.close()
+            connection.close(reply_code="200", reply_text="Gracefully closed connection")
             print("Got message ? {}".format(body))
             return 'ml_workflow'
     else:
+        connection.close(reply_code="200", reply_text="Gracefully closed connection")
         return 'task_trash'
 
 
