d856b67db0c1ca57db4eaa8251a77cc893fe12f8
mohittalele <mohittalele@gmail.com>
2022-09-01 15:06:07 +0200

    Update tutorial_etl_dag.py

--- DAG/tutorial_etl_dag.py
+++ DAG/tutorial_etl_dag.py
@@ -38,7 +38,7 @@ from airflow.operators.python import PythonOperator
 
 # [START instantiate_dag]
 with DAG(
-    'tutorial_etl_dag',
+    'tutorial_etl_dag_old',
     # [START default_args]
     # These args will get passed on to each operator
     # You can override them on a per-task basis during operator initialization
