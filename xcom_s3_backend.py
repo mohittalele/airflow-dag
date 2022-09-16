e1c5de19d373114fefed95af99ca410199f7749d
Mohit Talele <mohit.talele@rtl2.de>
2022-09-16 15:05:04 +0200

    add pickling xcom

--- xcom_s3_backend.py
+++ xcom_s3_backend.py
@@ -5,6 +5,7 @@ import pandas as pd
 from typing import Any
 from airflow.models.xcom import BaseXCom
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from typing import Optional
 
 
 class S3XComBackend(BaseXCom):
@@ -78,5 +79,85 @@ class S3XComBackend(BaseXCom):
                     local_path="/tmp"
                 )
                 loaded_dict[df_key] = pd.read_csv(filename)
+            result = loaded_dict
+        return result
+
+
+class S3XComBackendPickling(BaseXCom):
+    PREFIX = "s3"
+    BUCKET_NAME = os.environ.get("S3_XCOM_BUCKET_NAME")
+    CONN_ID = os.environ.get("S3_XCOM_CONN_ID")
+
+    @staticmethod
+    def _assert_s3_backend():
+        if S3XComBackendPickling.BUCKET_NAME is None:
+            raise ValueError("Unknown bucket for S3 backend.")
+
+    @staticmethod
+    def serialize_value(value: Any,
+                        key: Optional[str] = None,
+                        task_id: Optional[str] = None,
+                        dag_id: Optional[str] = None,
+                        run_id: Optional[str] = None,
+                        map_index: Optional[int] = None):
+        if isinstance(value, pd.DataFrame):
+            S3XComBackendPickling._assert_s3_backend()
+            hook = S3Hook(aws_conn_id=S3XComBackendPickling.CONN_ID)
+            s3key = f"data_{str(uuid.uuid4())}.pickle"
+            filename = f"{s3key}.pickle"
+            value.to_pickle(filename)
+            hook.load_file(
+                filename=filename,
+                key=s3key,
+                bucket_name=S3XComBackendPickling.BUCKET_NAME,
+                replace=True
+            )
+            value = f"{S3XComBackendPickling.PREFIX}://{S3XComBackendPickling.BUCKET_NAME}/{s3key}"
+
+        elif isinstance(value, dict) and all(isinstance(x, pd.DataFrame) for x in value.values()):
+            S3XComBackendPickling._assert_s3_backend()
+            hook = S3Hook(aws_conn_id=S3XComBackendPickling.CONN_ID)
+            stripped_dict = {}
+            for df_key, df_value in value.items():
+                s3key = f"data_{str(uuid.uuid4())}.csv"
+                filename = f"{s3key}.csv"
+                df_value.to_pickle(filename)
+                hook.load_file(
+                    filename=filename,
+                    key=s3key,
+                    bucket_name=S3XComBackendPickling.BUCKET_NAME,
+                    replace=True
+                )
+                stripped_dict[df_key] = f"{S3XComBackendPickling.PREFIX}://{S3XComBackendPickling.BUCKET_NAME}/{s3key}"
+            value = stripped_dict
+
+        return BaseXCom.serialize_value(value)
+
+    @staticmethod
+    def deserialize_value(result) -> Any:
+        result = BaseXCom.deserialize_value(result)
+
+        if isinstance(result, str) and result.startswith(S3XComBackendPickling.PREFIX):
+            S3XComBackendPickling._assert_s3_backend()
+            hook = S3Hook(aws_conn_id=S3XComBackendPickling.CONN_ID)
+            s3key = result.replace(f"{S3XComBackendPickling.PREFIX}://{S3XComBackendPickling.BUCKET_NAME}/", "")
+            filename = hook.download_file(
+                key=s3key,
+                bucket_name=S3XComBackendPickling.BUCKET_NAME,
+                local_path="/tmp"
+            )
+            result = pd.read_pickle(filename)
+        elif isinstance(result, dict) and all(x.startswith(S3XComBackendPickling.PREFIX) for x in result.values()):
+            S3XComBackendPickling._assert_s3_backend()
+            hook = S3Hook(aws_conn_id=S3XComBackendPickling.CONN_ID)
+            loaded_dict = {}
+            for df_key, df_value in result.items():
+                s3key = df_value.replace(f"{S3XComBackendPickling.PREFIX}://{S3XComBackendPickling.BUCKET_NAME}/", "")
+                filename = hook.download_file(
+                    key=s3key,
+                    bucket_name=S3XComBackendPickling.BUCKET_NAME,
+                    local_path="/tmp"
+                )
+                loaded_dict[df_key] = pd.read_pickle(filename)
             result = loaded_dict
         return result
